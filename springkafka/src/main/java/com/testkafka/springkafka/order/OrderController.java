04990e98370c8beb94d5e19378145cea37cb6151
rolle_dev <89302745+toanhhg123@users.noreply.github.com>
2024-12-27 23:52:35 +0700

    chore: add config kafka

---
+++ springkafka/src/main/java/com/testkafka/springkafka/order/OrderController.java
@@ -0,0 +1,12 @@
+package com.testkafka.springkafka.order;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/order")
+public class OrderController {
+}
+
+
+
